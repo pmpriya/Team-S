149c360213d63ceeff78a1e50bd8fc43af5c34c4
ligia26 <ligia.roxana26@gmail.com>
2020-02-22 20:07:23 +0000

    db connection attempt

--- private/initialise.php
+++ private/initialise.php
@@ -12,8 +12,11 @@
   $public_end = strpos($_SERVER['SCRIPT_NAME'], '/public') + 7;
   $doc_root = substr($_SERVER['SCRIPT_NAME'], 0, $public_end);
   define("WWW_ROOT", $doc_root);
-
+  require_once('database.php');
+  require_once('query_functions.php');
   require_once('functions.php');
+
+  $db = db_connect();
   $errors = [];
 
 ?>
