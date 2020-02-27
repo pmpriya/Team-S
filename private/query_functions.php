b601bdd8b5f685c4b7d9888fc8bb4f38c8742a4d
ligia26 <ligia.roxana26@gmail.com>
2020-02-27 02:08:37 +0000

    database connection successful

--- private/query_functions.php
+++ private/query_functions.php
@@ -5,28 +5,28 @@
   $sql = "SELECT * FROM Patient ";
   $sql .= "WHERE nhs_number='" . $nhs_number . "' ";
   $result = mysqli_query($db, $sql);
-  confirm_result_set($result);
-  $member = mysqli_fetch_assoc($result);
-  mysqli_free_result($result);
-  return $member;
+ 
+  return $result;
 }
-function insert_member($firstname, $lastname, $dob,$nhsnumber, $mobilephone,$homephone, $address, $gender,$nhs_number,$gp_address,$gp_number) {
+function insert_member($nhs_number, $first_name, $last_name, $dob, $sex, $home_address, $postcode, $home_phone, $mobile_phone, $gp_address, $gp_number) {
     global $db;
 
     $sql = "INSERT INTO Patient ";
-    $sql .= "(firstname, lastname, dob, phone, address, gender, email) ";
+    $sql .= "(nhs_number, first_name, last_name, date_of_birth, sex, home_address, postcode, home_phone, mobile_phone, gp_address, gp_phone) ";
     $sql .= "VALUES (";
-    $sql .= "'" . $firstname . "', ";
-    $sql .= "'" . $lastname . "', ";
-    $sql .= "'" . $dob . "', ";
-    $sql .= "'" . $nhsnumber . "', ";
-    $sql .= "'" . $mobilephone . "', ";
-    $sql .= "'" . $homephone . "', ";
-    $sql .= "'" . $address . "', ";
-    $sql .= "'" . $gender . "', ";
     $sql .= "'" . $nhs_number . "', ";
+    $sql .= "'" . $first_name . "', ";
+    $sql .= "'" . $last_name . "', ";
+    $sql .= "'" . $dob . "', ";
+    $sql .= "'" . $sex . "', ";
+ 
+    $sql .= "'" . $home_address . "', ";
+    $sql .= "'" . $postcode . "', ";
+    $sql .= "'" . $home_phone . "', ";
+    $sql .= "'" . $mobile_phone . "', ";
+    //$sql .= "'" . $nhs_number . "', ";
     $sql .= "'" . $gp_address . "', ";
-    $sql .= "'" . $gp_number . "', ";
+    $sql .= "'" . $gp_number . "'";
     $sql .= ")";
     $result = mysqli_query($db, $sql);
     if($result) {
@@ -37,5 +37,6 @@ function insert_member($firstname, $lastname, $dob,$nhsnumber, $mobilephone,$hom
       exit;
     }
   }
+  
 
   ?>
