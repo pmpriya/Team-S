7c9a06f04007083069c6ef453414c49c7a51fd6b
OsmanPoonawala <45496552+OsmanPoonawala@users.noreply.github.com>
2020-03-13 13:00:05 +0000

    Add files via upload

--- public/passwordReset.php
+++ public/passwordReset.php
@@ -27,13 +27,54 @@
                <h1>Edit user</h1>
 
 
-                            <form method="post">
+                            <form method="post" id="form">
                                 <div class="form-group" align="center">
+                                    <span id="alert_message" style="color:red"></span>
+
                                     <table>
-                                        <tr><td>New password:</td><td> <textarea  name="password" rows="1" cols="10"></textarea></td></tr>
+                                        <tr><td>New Password:</td><td> <textarea   required=""  id="password"  onfocusout="isEmpty(this,'New Password')" name="password" rows="1" cols="10"></textarea></td></tr>
                                     </table>
 
-                                <button type="submit" class="btn btn-sm btn-primary"><i class="far fa-save"></i>Update password</button>
+                                <button type="button" onclick="validateForm()" class="btn btn-sm btn-primary"><i class="far fa-save"></i>Update password</button>
                             </form>
                       </center>
 <?php include(SHARED_PATH . '/footer.php'); ?>
+
+        <script type="text/javascript">
+    var append = false;
+</script>
+<script type="text/javascript">
+    function isEmpty(r,e){
+       if(r.value.trim()==""){
+            if(append)
+                document.getElementById("alert_message").innerHTML += e+" can't be empty.</br>";
+            else
+                document.getElementById("alert_message").innerHTML =e+" can't be empty";
+            return true;
+       }
+       if(append) 
+            document.getElementById("alert_message").innerHTML += "";
+        else
+            document.getElementById("alert_message").innerHTML = "";
+       return false;
+    }
+</script>
+   
+
+<script type="text/javascript">
+    function validateForm(){
+        document.getElementById("alert_message").innerHTML = "";
+        var password = document.getElementById("password");
+
+        append = true;
+       
+
+        if(!isEmpty(password,"password")){
+            document.getElementById("form").submit();
+
+            return true;
+        }
+        append = false;
+        return false;
+    }
+</script>
