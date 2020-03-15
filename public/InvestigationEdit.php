ecb62a889a25c2a6e153ed4ef415ba91be329c02
eiriniretzepi <46709382+eiriniretzepi@users.noreply.github.com>
2020-03-15 16:04:24 +0000

    change links

--- public/InvestigationEdit.php
+++ public/InvestigationEdit.php
@@ -1,5 +1,5 @@
-<?php require_once ('../../private/initialise.php'); 
-include('../../private/shared/header.php'); 
+<?php require_once ('../private/initialise.php'); 
+include('../private/shared/header.php'); 
 // if(!isset($GET['patient_ID'])){
 //     redirect_to(url_for('/investigations/show.php?patient_ID= ' . $patient_ID));
 // }
@@ -23,7 +23,7 @@ echo $patient_ID;
 
         <?php while ($date = mysqli_fetch_assoc($date_set)){ ?>
             <tr>
-                <td> <a class="actions" href = "<?php echo url_for('/investigations/edit_by_date.php?patient_ID=' . $patient_ID .'&date='. $date['date']); ?> " ><?php echo h($date['date']);?> </a></td> 
+                <td> <a class="actions" href = "<?php echo url_for('/InvestigationsEditByDate.php?patient_ID=' . $patient_ID .'&date='. $date['date']); ?> " ><?php echo h($date['date']);?> </a></td> 
             </tr> 
         <?php } ?>
     </table>
