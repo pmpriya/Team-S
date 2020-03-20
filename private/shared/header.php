bf81728b74466c82347ef363d9c4446b4d88876b
ligia26 <55756920+ligia26@users.noreply.github.com>
2020-03-20 13:56:32 +0000

    Update header.php

--- private/shared/header.php
+++ private/shared/header.php
@@ -17,14 +17,14 @@
       <li><a href="<?php echo url_for('/index.php'); ?>">MAIN</a></li>
       
       <li><a href="<?php echo url_for('/register_patient.php'); ?>">REGISTER PATIENT</a></li> 
-      <li><a href="<?php echo url_for('/user_login.php'); ?>">LOG IN</a></li>
+     
         <li><a href="<?php echo url_for('/users.php'); ?>">STAFF</a></li>
        
         <li><a href="<?php echo url_for('/patients.php'); ?>">PATIENTS</a></li>
 
         <li><a href="<?php echo url_for('/register_member.php'); ?>">REFERRALS </a></li>
         <li><a href="<?php echo url_for('/external_access.php'); ?>">STATUS</a></li>
-
+        <li><a href="<?php echo url_for('/user_login.php'); ?>">LOG IN</a></li>
 
     </ul>
   </navigation>
