bca21a42cd551fe1135e668de10019aab5260d0e
OsmanPoonawala <45496552+OsmanPoonawala@users.noreply.github.com>
2020-03-22 20:40:21 +0500

    Update register_patient.php

--- public/register_patient.php
+++ public/register_patient.php
@@ -391,11 +391,11 @@ $val = isOnlyNumber($mobile_phone);
                     document.getElementById("alert_message").innerHTML = e+" must have more than equal to 2 characters";
                 return false;
             }
-            if(r.value.length>10){
+            if(r.value.length>30){
                 if(append)
-                    document.getElementById("alert_message").innerHTML += e+" must have less than equal to 10 characters<br/>";
+                    document.getElementById("alert_message").innerHTML += e+" must have less than equal to 30 characters<br/>";
                 else
-                    document.getElementById("alert_message").innerHTML = e+" must have less than equal to 10 characters";
+                    document.getElementById("alert_message").innerHTML = e+" must have less than equal to 30 characters";
                 return false;
             }
             if (/^([a-zA-Z]+\s)*[a-zA-Z]+$/.test(r.value.trim()))
