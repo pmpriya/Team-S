a189e3426b1888a5a4c578870beda349006c2dac
OsmanPoonawala <45496552+OsmanPoonawala@users.noreply.github.com>
2020-03-22 18:55:15 +0500

    Update validation.php

--- private/shared/validation.php
+++ private/shared/validation.php
@@ -40,3 +40,49 @@
             }
             return 6;   
     }
+
+    function ValidateClientEmail($var) 
+    {
+    	if(!isset($var)||empty($var))
+				return 7;
+
+        if (preg_match("/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/",$var))
+        {
+
+            return 1;
+
+        }
+
+        return 6;    
+        
+    }
+
+
+	function getMessage($value,$title=""){
+        
+		switch ($value) {
+			case '2':
+				return $title." must have more than equal to 2 characters<br/>";
+				break;
+			case '3':
+				return $title." must have less than equal to 10 characters<br/>";
+				break;
+			case '4':
+				return $title." can only contain Characters<br/>";
+				break;
+			case '5':
+				return $title." can only contain Numbers<br/>";
+				break;
+			case '6':
+				return "Invalid Email<br/>";
+				break;
+			case '7':
+				return "Emptry String<br/>";
+				break;
+			default:
+				
+				# code...
+				break;
+		}
+	}
+?>
