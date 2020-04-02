5f0860e5e9cf73a74faac1ddd1166f38d8990729
K1895306 <47645897+k1895306@users.noreply.github.com>
2020-04-02 23:35:07 +0530

    Update user_login.php

--- public/user_login.php
+++ public/user_login.php
@@ -28,7 +28,7 @@ if(!isset($password) || empty($password)){
     // User exists
     if($user) {
     
-      if(md5($password) == $user['password']) {
+      if(password_verify($password, $user['password'])) {
       
         log_in_user($user);
         redirect_to(url_for('patients.php'));
