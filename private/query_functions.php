fc15f3adc0949c41bb6dfd286b41afbb2ebdf50a
ligia26 <55756920+ligia26@users.noreply.github.com>
2020-04-08 21:58:55 +0100

    remove send mail from query functions

--- private/query_functions.php
+++ private/query_functions.php
@@ -83,52 +83,6 @@ $ref_dr_name,$ref_email,$ref_hospital_name,$reg_surname,$reg_forename,$reg_email
     }
 }
 
-function send_mail_ref_doctor($ref_email,$first_name,$last_name,$nhs_number,$accessCode){
-
-    
-  $to  = $ref_email; 
-  $subject = 'Signup verification'; 
-  $message = '
-   
-  As a patient of yours: '.$first_name . $last_name. '  has been registered with Kings College Health Centre - Paediatric Liver Section, you have been created a KCH account automatically.
-  You can now login with your username and password with the credentials below:
-   
-  ------------------------
-  NHS number: '.$nhs_number.'
-  Password: '.$accessCode.'
-  ------------------------
-   
-  Please log in to your account if you want to view or edit data of your newly registered patient:
-  http://project.juliusz.uk/public/external_access.php
-   
-  '; 
-                       
-  $headers = 'From:noreply@nhs.net' . "\r\n"; 
-  mail($to, $subject, $message, $headers); 
-}
-
-function send_mail_registration($email,$first_name){
-    $subject = 'Patient registration'; 
-    $message = '
-
-       Hello '.$first_name.',
-        
-       We are happy to let you know that you have been fully registered with Kings College Health Centre - Paediatric Liver Section.
-       Now we have access to your investigations and personal data and you will be notified by email before every appointment with us.
-
-
-       Kind Regards,
-
-       Kings College London NHS Health Centre
-       
-      
-
-       ';
-    $to = $email; 
-    $headers = 'From:noreply@nhs.net' . "\r\n"; 
-    mail($to, $subject, $message, $headers); 
-  
-  }
 function find_all_users() 
 {
     global $db;
