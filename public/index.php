e0c7a968f639d35c23e35b4478863cefc0939d27
K1895306 <47645897+k1895306@users.noreply.github.com>
2020-04-09 00:35:48 +0530

    Update index.php

--- public/index.php
+++ public/index.php
@@ -3,13 +3,22 @@
 <?php include(SHARED_PATH . '/header.php'); ?>
 <?php $page_title = 'KCL Paedriatic Liver Service'; ?>
 
-    <div class="public">
-
-        <title>KCL Paedriatic Liver Service</title>
-        <br>
-
-        <img src="images/kcl.jpg" alt="Logo" id="logo">
+   <img src ="images/doctor.jpg" alt="main_image" id="doctor"> 
     
-    </div>
+ <h1 id="aboutus"> ABOUT US </h1>
+ <p id="about"> King's Paediatric Liver Centre is a national hub providing a highly specialised service to children with liver problems.
+     We are the largest service of its type in the world.
+     We assess and treat children with: </p>
+   <ul id="listofservice">
+    <li> neonatal (newborn) liver diseases </li>
+    <li> chronic (long-standing) liver disease </li>
+    <li> acute liver failure </li>
+</ul>
+<p>We also offer surgery for liver tumours and pancreas conditions, and carry out liver and small bowel transplantations. </p>
+
+<img class = "image-aboutus" src ="images/image-1.jpg" alt="image-1" id="image1"> 
+<img class = "image-aboutus" src ="images/image-2.jpg" alt="image-2" id="image2"> 
+<img class = "image-aboutus" src ="images/image-3.jpg" alt="image-3" id="image3"> 
+<img class = "image-aboutus" src ="images/image-4.jpg" alt="image-4" id="image4"> 
 
 <?php include(SHARED_PATH . '/footer.php'); ?>
