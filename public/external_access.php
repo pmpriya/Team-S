fd0f0740110acf444edd776163e344dd3d4b73b1
K1895306 <47645897+k1895306@users.noreply.github.com>
2020-04-11 16:11:07 +0530

    Update external_access.php

--- public/external_access.php
+++ public/external_access.php
@@ -47,13 +47,4 @@
     </div>
     </form>
     </div>
-<style>
-    #label {
-    font-size: 13px;
-    font-weight: bold;
-    color : rgb(42,103,204);
-    font-family: 'Open Sans', sans-serif;
-    text-align: center;
-    }
-    </style>
             <?php include(SHARED_PATH . '/footer.php'); ?>
