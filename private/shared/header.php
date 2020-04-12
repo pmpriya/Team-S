52764eced3264502025f05120245a842dcba0e58
eiriniretzepi <ireneretzepis@yahoo.com>
2020-04-13 02:52:48 +0300

    changed header

--- private/shared/header.php
+++ private/shared/header.php
@@ -21,69 +21,69 @@
         <?php
         if (isset($_SESSION['userLevel'])) {
             if(!$_SESSION['userLevel'] == 1) {
-                echo '<li><a href=register_patient.php>REGISTER PATIENT</a></li>';
+                echo '<li><a href='.WWW_ROOT.'/patient/new.php>REGISTER PATIENT</a></li>';
             }        else{
-            echo '<li><a href=register_patient.php>REGISTER PATIENT</a></li>';}
+            echo '<li><a href='.WWW_ROOT.'/patient/new.php>REGISTER PATIENT</a></li>';
         }elseif (isset($_SESSION['nhsno'])) {}
 
         else{
-            echo '<li><a href=register_patient.php>REGISTER PATIENT</a></li>';
+            echo '<li><a href='.WWW_ROOT.'/patient/new.php>REGISTER PATIENT</a></li>';
         }
         ?>
 
         <?php
         if (isset($_SESSION['userLevel'])) {
             if($_SESSION['userLevel'] > 1)
-                echo '<li><a href=activeCases.php>ACTIVE CASES</a></li>';
+                echo '<li><a href='.WWW_ROOT.'/activeCases.php>ACTIVE CASES</a></li>';
         }
         ?>
 
         <?php
         if (isset($_SESSION['userLevel'])) {
             if($_SESSION['userLevel'] > 0)
-                echo '<li><a href=patients.php>PATIENTS</a></li>';
+                echo '<li><a href='.WWW_ROOT.'/patient/index.php>PATIENTS</a></li>';
         }
         ?>
 
         <?php
         if (isset($_SESSION['userLevel'])||isset($_SESSION['nhsno'])) {
         }else{
-                echo '<li><a href=user_login.php>STAFF LOGIN</a></li>';
+            echo '<li><a href='.WWW_ROOT.'user_login.php>STAFF LOGIN</a></li>';
         }
         ?>
 
         <?php
         if (isset($_SESSION['userLevel'])||isset($_SESSION['nhsno'])) {
             }else{
-            echo '<li><a href=external_access.php>REFEREE LOGIN</a></li>';
+            echo '<li><a href='.WWW_ROOT.'external_access.php>REFEREE LOGIN</a></li>';
         }
         ?>
         
          <?php
         if (isset($_SESSION['userLevel'])) {
             if($_SESSION['userLevel'] > 0)
-                echo '<li><a href=appointments.php>APPOINTMENTS</a></li>';
+                echo '<li><a href='.WWW_ROOT.'/appointment/index.php>APPOINTMENTS</a></li>';
         }
         ?>
 
         <?php
        if (isset($_SESSION['userLevel'])) {
            if($_SESSION['userLevel'] > 2){
-           echo '<li><a href=users.php>STAFF</a></li>';
+               echo '<li><a href='.WWW_ROOT.'/user/index.php>STAFF</a></li>';
        }}
         ?>
 
 
         <?php
         if (isset($_SESSION['nhsno'])) {
-                echo '<li><a href=externalOverview.php>MY CASE</a></li>';
+            echo '<li><a href='.WWW_ROOT.'externalOverview.php>MY CASE</a></li>';
         }
         ?>
 
 
 <?php
         if (isset($_SESSION['userLevel']) || isset($_SESSION['nhsno'])) {
-                echo '<li><a href=user_logout.php>LOGOUT</a></li>';
+             echo '<li><a href='.WWW_ROOT.'user_logout.php>LOGOUT</a></li>';
         }
         ?>
 
