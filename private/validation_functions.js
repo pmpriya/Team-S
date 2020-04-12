7e3e6cfc6ee6e4019ac3162203937d02cdadc901
noorbaakza <55815113+noorbaakza@users.noreply.github.com>
2020-04-13 01:26:22 +0500

    Update validation_functions.js

--- private/validation_functions.js
+++ private/validation_functions.js
@@ -83,3 +83,91 @@ function isEmpty(r, e) {
   else document.getElementById("alert_message").innerHTML = "";
   return false;
 }
+
+function isOnlyCharacter(r, e) {
+  if (!isEmpty(r, e)) {
+    if (r.value.length < 1) {
+      if (append)
+        document.getElementById("alert_message").innerHTML +=
+          e + " must have more than equal to 1 characters<br/>";
+      else
+        document.getElementById("alert_message").innerHTML =
+          e + " must have more than equal to 1 characters";
+      return false;
+    }
+    if (r.value.length > 30) {
+      if (append)
+        document.getElementById("alert_message").innerHTML +=
+          e + " must have less than equal to 30 characters<br/>";
+      else
+        document.getElementById("alert_message").innerHTML =
+          e + " must have less than equal to 30 characters";
+      return false;
+    }
+    if (/^([a-zA-Z]+\s)*[a-zA-Z]+$/.test(r.value.trim())) {
+      if (append) document.getElementById("alert_message").innerHTML += "";
+      else document.getElementById("alert_message").innerHTML = "";
+      return true;
+    }
+    if (append)
+      document.getElementById("alert_message").innerHTML +=
+        e + " can only contain characters<br/>";
+    else
+      document.getElementById("alert_message").innerHTML =
+        e + " can only contain characters";
+    return false;
+  }
+  return false;
+}
+
+function isOnlyNumber(r, e) {
+  if (!isEmpty(r, e)) {
+    if (/^\d+$/.test(r.value.trim())) {
+      if (append) document.getElementById("alert_message").innerHTML += "";
+      else document.getElementById("alert_message").innerHTML = "";
+      return true;
+    }
+    if (append)
+      document.getElementById("alert_message").innerHTML +=
+        e + " can only contain Numbers<br/>";
+    else
+      document.getElementById("alert_message").innerHTML =
+        e + " can only contain Numbers";
+    return false;
+  }
+  return false;
+}
+
+function ValidateEmail() {
+  var mail = document.getElementById("email");
+  if (!isEmpty(mail, "Mail")) {
+    mail = mail.value;
+    if (/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(mail)) {
+      if (append) document.getElementById("alert_message").innerHTML += "";
+      else document.getElementById("alert_message").innerHTML = "";
+      return true;
+    }
+    if (append)
+      document.getElementById("alert_message").innerHTML += "Invalid Email";
+    else document.getElementById("alert_message").innerHTML = "Invalid Email";
+    return false;
+  }
+  return false;
+}
+
+// function validateDate(signup, e) {
+//   signup = signup.value;
+//   var reg = /^\s*(3[01]|[12][0-9]|0?[1-9])\-(1[012]|0?[1-9])\-((?:19|20)\d{2})\s*$/;
+//   if (reg.test(signup)) {
+//     if (append) document.getElementById("alert_message").innerHTML += "";
+//     else document.getElementById("alert_message").innerHTML = "";
+//     return true;
+//   }
+//   if (append)
+//     document.getElementById("alert_message").innerHTML +=
+//       e + " should be in DD/MM/YYYY format";
+//   else
+//     document.getElementById("alert_message").innerHTML =
+//       e + " should be in DD/MM/YYYY format";
+//   return false;
+// }
