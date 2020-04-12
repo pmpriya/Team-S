681543d90beed0e211cc3ffdd5ed048f55978e95
TheJot <julek175j@gmail.com>
2020-04-12 20:50:02 +0100

    Implement tracking referrals
    Signed-off-by: TheJot <julek175j@gmail.com>

--- public/referral_show.php
+++ public/referral_show.php
@@ -19,6 +19,7 @@
     <br>
 
         <?php  if(!mysqli_num_rows(access_actve_referral($patient_ID))){ ?>
+            This patient has no ongoing referrals at the moment, please use the link below to start a new case.<br><br>
         <a href=referral_page.php?id=<?php echo $patient_ID ?>>Create Referral</a><br>
         <?php }?>
         <br>
@@ -28,7 +29,7 @@
         <th> Consultant name</th> 
         <th> Consultant Specialty</th>
         <th> Organisation Hospital Name</th>
-
+        <th> Status</th>
         <th> View </th>
  
      
@@ -41,15 +42,15 @@
                 <td><?php echo h($allReferrals['consultant_name']); ?></td>
                 <td><?php echo h($allReferrals['consultant_specialty']); ?></td>
                 <td><?php echo h($allReferrals['organisation_hospital_name']); ?></td>
+                <td><?php if($allReferrals['Active']==1){echo "<b><font color=green>IN PROGRESS</b></font>";} else{echo "<b><font color=Red>RESOLVED</b></font>";} ?></td>
                 <td><a href=referral_details.php?id=<?php echo h($allReferrals['ID']); ?>>Details</a></td>
             
             </tr> 
         <?php } ?>
     </table>
-
+        <td><?php echo h($allReferrals['Active']); ?></td>
 
     </center>
-
 
 
     </div>
