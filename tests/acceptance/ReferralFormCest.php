4b9d05d643a52139c537c0dfabf068f20a3aea50
eiriniretzepi <46709382+eiriniretzepi@users.noreply.github.com>
2020-04-13 16:07:49 +0300

    change links

--- tests/acceptance/ReferralFormCest.php
+++ tests/acceptance/ReferralFormCest.php
@@ -2,29 +2,52 @@
 
 class ReferralFormCest
 {
-   /* public function _before(AcceptanceTester $I)
+    public function _before(AcceptanceTester $I)
     {
         $I->amOnPage('/user_login.php');
         $I->fillField('username','admin');
         $I->fillField('password','admin');
         $I->click('Sign in');
+        
+        $randomNumber = mt_rand(1111111111,9999999999);
+        
+        $I->wantTo("add a patient");
+        $I->amOnPage('/patient/new.php');
+        $I->fillField('lastname2','testreg');
+        $I->fillField('firstname2','testreg');
+        $I->fillField('mail2','testing@nhs.net');
+        $I->fillField('lastname','test');
+        $I->fillField('firstname','reg');
+        $I->fillField('nhsnumber',$randomNumber);
+        $I->fillField('dob',10/10/1999);
+        $I->fillField('refname','Test');
+        $I->fillField('ref_mail','tes'. $randomNumber .'@nhs.com');
+        $I->fillField('refhospital','Royal Free Test Hospital');
+   
+        $I->fillField('email','hash4im53' . $randomNumber .'@gmail.com');
+        $I->fillField('address','Test Lane 5');
+        $I->fillField('postcode',39460);
+        $I->fillField('homenumber',07245163);
+        $I->fillField('mobilenumber',072565163);
+        $I->fillField('gpaddress','Test Street');
+        $I->fillField('gpnumber',07245166);
+        $I->click('Submit');
     }
     
     public function referralIncompleteSubmitTest(AcceptanceTester $I)
     {
-        $I->amOnPage('/new.php?id=141');
-                
-
+                 $I->canSeeInCurrentUrl('/referral_page.php');
                 
                 $I->click('Submit');
-                $I->see("REFERRAL FORM for Test Test");
-                $I->canSeeInCurrentUrl('/new.php?id=141');
+                $I->see("REFERRAL FORM for reg test");
+                $I->canSeeInCurrentUrl('/referral/new.php');
+                $I->cantSee("new investigation");
                 
              
     }
     public function referralSuccessfulSubmitTest(AcceptanceTester $I)
     {
-                $I->amOnPage('/new.php?id=141');
+                $I->canSeeInCurrentUrl('/referral/new.php');
                 $I->fillField('consultant_name','testname');
                 $I->fillField('consultant_specialty','testspecialty');
                 $I->fillField('organisation_hospital_name','testhosname');
@@ -43,8 +66,8 @@ class ReferralFormCest
                 
                 $I->click('Submit');
 
-                $I->see('Referrals for Test Test');
-                $I->see('testname');
+                $I->cantSeeInCurrentUrl('/referral/show.php');
+                $I->see('Investigation');
                 
 
              
@@ -53,6 +76,5 @@ class ReferralFormCest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
-    } */
-    
+    }
 }
